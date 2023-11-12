80f0f5c8f9d19e7335aa87782dae46fc77a33d38
quynhcompa <len31078@gmail.com>
2023-11-12 22:52:51 +0700

    Cong

--- TinhToan/Form1.cs
+++ TinhToan/Form1.cs
@@ -6,5 +6,13 @@ namespace TinhToan
         {
             InitializeComponent();
         }
+
+        private void btcong_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txt1.Text);
+            double b = double.Parse(txt2.Text);
+            double c = a + b;
+            txtkq.Text = c.ToString();
+        }
     }
 }
